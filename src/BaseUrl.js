aecd5e65260e254434efd5597ab903be3b33327a
Shohanur Rahman Akash <akash.cse.diu@gmail.com>
2020-12-15 18:39:30 +0600

    Password reset live issue solved

--- src/BaseUrl.js
+++ src/BaseUrl.js
@@ -5,13 +5,9 @@ export let BASE_URL =
   window.location.hostname +
   "/foodkhan/laravel";
 
-// for foodkhan.com from localhost
+// for khadyo.com from localhost
 // export let BASE_URL = "http://foodkhan.com" + "/api/public";
 
-//for live server installation (Laravel React in same place)
+//for khadyo.com
 // export let BASE_URL =
-//   window.location.protocol + "//" + window.location.hostname;
-
-//for foodkhan.com
-// export let BASE_URL =
-//   window.location.protocol + "//" + window.location.hostname + "/laravel/";
+//   window.location.protocol + "//" + window.location.hostname + "/laravel";
